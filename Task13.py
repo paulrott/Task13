3776ee3bb4dab3d86b4c232a88a5b955babaced4
gromov.p <paulrott76@gmail.com>
2021-11-18 00:32:01 +0300

    Initial commit

---
+++ Task13.py
@@ -0,0 +1,49 @@
+# user_input = input('Enter any number')
+# if user_input == '7':
+#   print('7 is a lucky number! Today is your lucky day!')
+# else:
+#   print('Thank you! Have a nice day!')
+#
+# user_input = input('Enter any number')
+
+# weight = float(input('Enter your weight'))
+# height= float(input('Enter your height'))
+# BMI = weight/(height*height)
+#
+#
+# if BMI <= 18.4:
+#     print('Ваш индекс массы тела:')
+#     print(BMI)
+#     print( 'Дефицит массы тела')
+# elif BMI >= 18.5 and BMI <= 25:
+#     print('Ваш индекс массы тела:')
+#     print(BMI)
+#     print('Норма')
+# elif BMI >= 25.1 and BMI <= 30:
+#     print('Ваш индекс массы тела:')
+#     print(BMI)
+#     print('Предожирение')
+# elif BMI >= 30.1 and BMI <= 35:
+#     print('Ваш индекс массы тела:')
+#     print(BMI)
+#     print('Ожирение 1 степени')
+# elif BMI >= 35.1 and BMI <= 40:
+#     print('Ваш индекс массы тела:')
+#     print(BMI)
+#     print('Ожирение 2 степени')
+# elif BMI >= 40.1:
+#     print('Ваш индекс массы тела:')
+#     print(BMI)
+#     print('Ожирение 3 степени')
+
+
+number = input('Enter any number')
+if int(number) % 2 == 0:
+    print('The number is even')
+else:
+    print('The number is odd')
+
+
+
+
+
